fb42b4545f148ac3b1d60f71d1130fac83d1a81e
mb00t <mb00t@yandex.ru>
2021-07-20 13:56:17 +0300

    add DecodeSelf app for tests todos table

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,9 @@ module Main exposing (Msg(..), main, update, view)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Json.Decode as Json exposing (Decoder)
+import PostgRest as Rest exposing (Attribute, Request, Selection)
+import Schema
 
 
 main =
@@ -29,3 +32,23 @@ view model =
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
+
+
+type alias School =
+    { id : String
+    , name : String
+    }
+
+
+request : Request (List School)
+request =
+    Rest.readMany Schema.school
+        { select = selection
+        }
+
+
+selection : Selection { a | id : Attribute String, name : Attribute String } School
+selection =
+    Rest.map2 School
+        (Rest.field .id)
+        (Rest.field .name)
